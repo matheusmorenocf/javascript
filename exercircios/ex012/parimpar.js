4ee856d8b822d5fd68e8fcb0453498f46e8ba2b0
Matheus Moreno <111401512+matheusmorenocf@users.noreply.github.com>
2022-09-03 12:40:20 -0300

    Update parimpar.js

--- exercircios/ex012/parimpar.js
+++ exercircios/ex012/parimpar.js
@@ -4,6 +4,6 @@ function parImpar(){
     if (num%2 == 0){
         resultado.innerHTML = `O número ${num} que foi digitado é <strong>PAR!</strong> `
     } else{
-        resultado.innerHTML = `O número ${num} que foi digitado é <strong>IMPAR!</strong> `
+        resultado.innerHTML = `O número ${num} que foi digitado é <strong>ÍMPAR!</strong> `
     }
 }
