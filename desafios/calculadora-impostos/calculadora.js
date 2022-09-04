bc8433c8b0813b46f816eb3594536b30c6938aef
Matheus Moreno <111401512+matheusmorenocf@users.noreply.github.com>
2022-09-04 12:26:48 -0300

    att

--- desafios/calculadora-impostos/calculadora.js
+++ desafios/calculadora-impostos/calculadora.js
@@ -1,16 +1,16 @@
 function calcular(){
-    let preco = Number(document.getElementById('precoB').value)
-    let ipi = Number(document.getElementById('percIpi').value)
-    let icms = Number(document.getElementById('percIcms').value)
+    let preco = Number(document.getElementById('txtpreco').value)
+    let ipi = Number(document.getElementById('percipi').value)
+    let icms = Number(document.getElementById('percicms').value)
     if(preco == 0 || icms == 0){
         alert('Valores insuficientes!')
     }else{
-        document.getElementById('valorIpi').innerHTML = preco * (ipi/100);
-        document.getElementById('valorIcms').innerHTML = preco * (icms/100);
-        document.querySelector('output#piscoff').innerHTML = preco * (9.25/100);
-        document.querySelector('output#precoBruto').innerHTML = preco +  (preco * (ipi/100));
-        document.getElementById('precoSicms').innerHTML = preco - (preco * (icms/100))
-        document.querySelector('output#precoLiquido').innerHTML = preco - (preco*(9.25/100));
+        document.getElementById('valorIpi').innerHTML = Number(preco * (ipi/100)).toFixed(2);
+        document.getElementById('valorIcms').innerHTML = Number( preco * (icms/100)).toFixed(2);
+        document.querySelector('output#piscoffins').innerHTML = Number(preco * (9.25/100)).toFixed(2);
+        document.querySelector('output#bruto').innerHTML = Number(preco +  (preco * (ipi/100))).toFixed(2);
+        document.getElementById('precoSicms').innerHTML = Number(preco - (preco * (icms/100))).toFixed(2);
+        document.querySelector('output#liquido').innerHTML = Number(preco - (preco*(9.25/100)) - (preco * (icms/100))).toFixed(2);
     }
     
 
